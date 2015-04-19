f14ec790f6f1753fb8b8717b2198c14665cc7add
Eiichi Sato <sato.eiichi@gmail.com>
2015-04-19 16:50:16 +0900

    handle EOF from underlying reader

--- bz2_reader.go
+++ bz2_reader.go
@@ -11,6 +11,7 @@ type Bz2Reader struct {
 	context C.bz_stream
 	reader  io.Reader
 	buf     []byte
+	eof     bool
 }
 
 func NewReader(reader io.Reader) (*Bz2Reader, error) {
@@ -18,35 +19,40 @@ func NewReader(reader io.Reader) (*Bz2Reader, error) {
 		reader: reader,
 		buf:    make([]byte, 1024),
 	}
-	if err := C.BZ2_bzDecompressInit(&result.context, 0, 0); err != 0 {
+	if err := C.BZ2_bzDecompressInit(&result.context, 0, 0); err != C.BZ_OK {
 		return nil, fmt.Errorf("%v", err)
 	}
 	return result, nil
 }
 
 func (self *Bz2Reader) Read(outbuf []byte) (int, error) {
-	if self.context.avail_in == 0 {
+	if self.context.avail_in == 0 && !self.eof {
 		n, err := self.reader.Read(self.buf[:])
 		if err != nil {
-			return 0, err
+			if err == io.EOF {
+				self.eof = true
+			} else {
+				return 0, err
+			}
 		}
 		self.context.next_in = (*C.char)(unsafe.Pointer(&self.buf[0]))
 		self.context.avail_in = C.uint(n)
 	}
 	self.context.next_out = (*C.char)(unsafe.Pointer(&outbuf[0]))
 	self.context.avail_out = C.uint(len(outbuf))
-	totalOutLo32 := self.context.total_out_lo32
-	totalOutHi32 := self.context.total_out_hi32
+
 	ret := C.BZ2_bzDecompress(&self.context)
 	if ret != C.BZ_OK && ret != C.BZ_STREAM_END {
 		return 0, fmt.Errorf("%v", ret)
 	}
-	outn := (uint64(self.context.total_out_hi32)<<32 | uint64(self.context.total_out_lo32)) - (uint64(totalOutHi32)<<32 | uint64(totalOutLo32))
-	if ret == C.BZ_STREAM_END {
-		return int(outn), io.EOF
-	} else {
-		return int(outn), nil
+	if ret == C.BZ_OK && self.eof && self.context.avail_in == 0 && self.context.avail_out > 0 {
+		return 0, io.ErrUnexpectedEOF
 	}
+	outn := len(outbuf) - int(self.context.avail_out)
+	if ret == C.BZ_STREAM_END {
+		return outn, io.EOF
+	}
+	return outn, nil
 }
 
 func (self *Bz2Reader) Close() error {
